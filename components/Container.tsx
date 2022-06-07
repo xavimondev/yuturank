4e68a27a9569943d0965342f88da8a50b79156e4
xavialfaro <nxb.developer@gmail.com>
2022-06-07 18:10:28 -0500

    initial setting up

---
+++ components/Container.tsx
@@ -0,0 +1,19 @@
+import styles from '../styles/Container.module.css';
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const Container = ({ children }: Props) => {
+  return (
+    <main className={styles.main}>
+      <h1 className={styles.main__title}>
+        <span className={styles.yutu__title}>Yutu</span>
+        <span>Rank</span>
+      </h1>
+      {children}
+    </main>
+  );
+};
+
+export default Container;
---
+++ components/Search.tsx
@@ -0,0 +1,22 @@
+import styles from '../styles/Search.module.css';
+import IconSearch from './Icon';
+
+const Search = () => {
+  return (
+    <>
+      <form className={styles.form}>
+        <div className={styles.form__label}>
+          <IconSearch className={styles.input__icon} />
+          <input
+            type='search'
+            name='search'
+            className={styles.input__field}
+            placeholder='Search your favorite youtube channel'
+          />
+        </div>
+      </form>
+    </>
+  );
+};
+
+export default Search;
